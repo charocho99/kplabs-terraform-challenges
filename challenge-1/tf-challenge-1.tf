0b8a2d3dbb124b477790830936e22050fde4c032
charocho99 <crc.19u10060@btech.nitdgp.ac.in>
2025-06-02 21:59:23 +0530

    challenge1_1

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,17 +1,23 @@
-provider "aws" {
-  version = "~> 2.54"
-  region  = "us-east-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-}
-
-provider "digitalocean" {}
-
 terraform {
-    required_version = "0.12.31"
+  required_version = "v0.12.31"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>2.54"
+    }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
 }
 
+provider "aws" {
+
+  region = "us-east-1"
+  #   access_key = "AKIAIOSFODNN7EXAMPLE"
+  #   secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+}
 
 resource "aws_eip" "kplabs_app_ip" {
-  vpc      = true
+  vpc = true
 }
